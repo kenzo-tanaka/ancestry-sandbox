fb994c07ba7396cc7b02b2a66aa7df916705b755
田中建蔵 <tanakakenzou@tanakakenzounoMacBook.local>
2019-11-03 18:16:09 +0900

    display default categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_categories, only: [:edit, :new]
   # GET /products
   # GET /products.json
   def index
@@ -70,5 +70,10 @@ class ProductsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :description)
+    end
+
+    def set_categories
+      @parent_categories = Category.roots
+      @default_child_categories = @parent_categories.first.children
     end
 end
