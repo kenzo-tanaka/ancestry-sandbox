15ea449b129acb62bd5abe1261df4200117ba412
田中建蔵 <tanakakenzou@tanakakenzounoMacBook.local>
2019-11-03 17:49:37 +0900

    generate product_category

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_084319) do
+ActiveRecord::Schema.define(version: 2019_11_03_084513) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -25,4 +34,6 @@ ActiveRecord::Schema.define(version: 2019_11_03_084319) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
